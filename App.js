e665c3a8a4e2c5bd909227b1547c361e7e419e50
Nam Nguyen <nikki.namnguyen12052002@gmail.com>
2023-05-04 15:19:02 +0700

    update home screen

--- App.js
+++ App.js
@@ -1,49 +1,49 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
-import Home from "./src/screens/Home";
-import Settings from "./src/screens/Settings";
-import Notifications from "./src/screens/Notifications";
-import Statistics from "./src/screens/Statistics";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import Home from './src/screens/Home';
+import Settings from './src/screens/Settings';
+import Notifications from './src/screens/Notifications';
+import Statistics from './src/screens/Statistics';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={Home}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				initialRouteName={Home}
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
 
-            switch (route.name) {
-              case "Home":
-                iconName = focused ? "home" : "home-outline";
-                break;
-              case "Statistics":
-                iconName = focused ? "bar-chart" : "bar-chart-outline";
-                break;
-              case "Notifications":
-                iconName = focused ? "notifications" : "notifications-outline";
-                break;
-              case "Settings":
-                iconName = focused ? "settings" : "settings-outline";
-                break;
-              default:
-                iconName = undefined;
-            }
+						switch (route.name) {
+							case 'Home':
+								iconName = focused ? 'home' : 'home-outline';
+								break;
+							case 'Statistics':
+								iconName = focused ? 'bar-chart' : 'bar-chart-outline';
+								break;
+							case 'Notifications':
+								iconName = focused ? 'notifications' : 'notifications-outline';
+								break;
+							case 'Settings':
+								iconName = focused ? 'settings' : 'settings-outline';
+								break;
+							default:
+								iconName = undefined;
+						}
 
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-        })}
-      >
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Statistics" component={Statistics} />
-        <Tab.Screen name="Notifications" component={Notifications} options={{ tabBarBadge: 3 }} />
-        <Tab.Screen name="Settings" component={Settings} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+						return <Ionicons name={iconName} size={size} color={color} />;
+					},
+					headerStyle: { backgroundColor: 'papayawhip' },
+				})}>
+				<Tab.Screen name="Home" component={Home} />
+				<Tab.Screen name="Statistics" component={Statistics} />
+				<Tab.Screen name="Notifications" component={Notifications} options={{ tabBarBadge: 3 }} />
+				<Tab.Screen name="Settings" component={Settings} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
---
+++ src/constants/colors.js
@@ -0,0 +1,4 @@
+export const COLOR_0 = '#23B371B3';
+export const COLOR_1 = '#31A9B8B3';
+export const COLOR_2 = '#D9C12AB3';
+export const COLOR_3 = '#F98866B3';
