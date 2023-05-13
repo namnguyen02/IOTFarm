e9ef8113434fc1f6c9d9e360b27bed048af4f205
Nam Nguyen <nikki.namnguyen12052002@gmail.com>
2023-05-13 21:30:26 +0700

    update statistic

--- src/screens/Statistics.js
+++ src/screens/Statistics.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions, ScrollView } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
+
+const screenWidth = Dimensions.get('window').width;
+
+const chartConfig = {
+	backgroundGradientFrom: '#FFFFFF',
+	backgroundGradientTo: '#FFFFFF',
+	decimalPlaces: 2,
+	color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+	barPercentage: 0,
+	useShadowColorFromDataset: true,
+};
+
+const data = {
+	labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+	datasets: [
+		{
+			data: [24, 25, 23, 30, 32, 27, 29],
+			color: (opacity = 1) => `rgba(35, 179, 113, ${opacity})`, // optional
+		},
+	],
+};
 
 export default function Settings() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Statistics Screen</Text>
-    </View>
-  );
+	return (
+		<View style={{ alignItems: 'center' }}>
+			<ScrollView>
+				<Text>Temperature</Text>
+				<LineChart
+					data={data}
+					width={0.9 * screenWidth}
+					height={256}
+					verticalLabelRotation={30}
+					chartConfig={chartConfig}
+					withDots={false}
+					bezier
+					style={{
+						marginVertical: 10,
+						borderRadius: 20,
+					}}
+				/>
+			</ScrollView>
+		</View>
+	);
 }
